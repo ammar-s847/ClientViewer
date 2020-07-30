faa1af28fad83405837d6bd1b5203f1d6494e54c
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2020-07-30 09:43:22 -0400

    added config-format and style.css

---
+++ scripts/config-format.php
@@ -0,0 +1,14 @@
+<?php
+
+define(SERVER, '');
+define(DATABASE, '');
+define(USERNAME, '');
+define(PASSWORD, '');
+
+$conn = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
+
+if ($conn->connect_errno) {
+    die("MySQL connection Error: " . $conn->connect_error);
+}
+
+?>
